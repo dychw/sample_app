3ce1f7b162ead714cbc397a7b7c34a6f034e4788
dychw <25680873+dychw@users.noreply.github.com>
2020-04-10 17:40:16 +0900

    Finish layout and routes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 require 'minitest/reporters'
 Minitest::Reporters.use!
@@ -7,6 +9,7 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
